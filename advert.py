2990dd20d427e6f22ddc669cdd673507d704fc1b
vsomani2 <vsomani2@illinois.edu>
2017-10-07 12:30:08 +0530

    Advert class

---
+++ advert.py
@@ -0,0 +1,30 @@
+import random
+
+class advert(object):
+	"""Class for advert"""
+	def __init__(self, category, expensive, clusterCat, location):
+		super(advert, self).__init__()
+		self.category = category
+		self.cost = cost
+		self.clusterCat = clusterCat
+		self.location = location
+		self.accepted = 0.0 # Number of times ad was clicked on
+		self.total = 0.0	# Total number of times ad was offered
+		self.availableClusters = [] # List of all cluster that this advert can switch to if it is rejeced too many times
+
+	def updateClusterCat(globalClusterRatio):
+		"""updates cluster category for the advertisement if too many people reject it"""
+		acceptRatio = self.accepted/self.total
+		if acceptRatio < globalClusterRatio:
+			self.availableClusters.remove(self.clusterCat)
+			self.clusterCat = random.choice(self.availableClusters)
+
+
+
+
+
+
+
+
+
+		
